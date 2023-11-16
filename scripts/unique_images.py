fec017e9196861cec4bc69314813877eb2b0a0b3
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-11-16 10:33:12 +0530

    feat: dhash unique images generation script

---
+++ scripts/unique_images.py
@@ -0,0 +1,31 @@
+"""
+    Find and save the unique (i.e, not filtered by DHash) images for one-shot-synthesis algorithm
+"""
+
+import os
+from imagededup.methods import DHash #noqa
+
+# Set up the DHash algorithm
+d_hasher = DHash()
+
+# Load the images and set the hamming distance threshold
+raw_images_path = "../data/processed/val/image"
+max_hamming_distance = 30
+
+# Find duplicates to remove
+duplicates_to_remove = d_hasher.find_duplicates_to_remove(
+    image_dir = raw_images_path,
+    max_distance_threshold = max_hamming_distance
+)
+
+# Find the unique images
+all_images = os.listdir(raw_images_path)
+unique_images = [f for f in all_images if f not in duplicates_to_remove]
+unique_images.sort()
+print(unique_images)
+
+# Save the unique images
+with open(f"unique_dhash_{max_hamming_distance}.txt", "w") as f:
+    for img_file_name in unique_images:
+        f.write(f"{img_file_name}\n")
+
